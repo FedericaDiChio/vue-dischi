a39ced022cf1913b6f4568550890b37fdd9b1d01
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-02 15:56:25 +0200

    axios

--- js/script.js
+++ js/script.js
@@ -4,6 +4,16 @@ Vue.config.devtools = true;
 
 const app = new Vue({
   el: '#app',
-  data: {},
+  data: {
+    music: "",
+  },
   methods: {},
+  created(){
+    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+      const response = res.data.response;
+      console.log(response)
+      this.music = response;
+
+    })
+  }
 });
