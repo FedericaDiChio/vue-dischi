c0d5970eeca9bc66e17a9e47b76802937eaf4140
FedericaDiChio <federica.dichio7@gmail.com>
2021-09-02 17:12:54 +0200

    sorted by year

--- js/script.js
+++ js/script.js
@@ -5,14 +5,21 @@ Vue.config.devtools = true;
 const app = new Vue({
   el: '#app',
   data: {
-    albums: "",
+    albums: [],
   },
   methods: {},
-  created(){
-    axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
-      const response = res.data.response;
-      // console.log(response)
-      this.albums = response;
-    })
-  }
-});
+  computed: {
+    sortedAlbums(){
+      return this.albums.sort((a, b) => a.year - b.year);
+      }
+    },
+    created(){
+      axios.get("https://flynn.boolean.careers/exercises/api/array/music").then((res) => {
+        const response = res.data.response;
+        // console.log(response)
+        this.albums = response;
+      })
+    }
+  });
+  
+  console.log(this.sortedAlbums)
